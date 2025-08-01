f2b1e66847bfb9b18bd6f4435ab59a5010f0a33f
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-08-01 22:17:47 +0800

    Set color themes, fonts, more init files

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,62 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import noot from './assets/nootnoot_photo.png'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Title />
+      <div className='justify-center items-center'>
+        <Label text='Your QPI' color='pink' />
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      
     </>
+  )
+}
+// this returns a label with rounded edges and background color
+function Label({text, color}){
+  return (
+    <div className={`text-white bg-${color} rounded-lg px-4 py-2 inline-block`}>
+      {text}
+    </div>
+  )
+}
+
+function Title(){
+  return(
+  <>
+      <div className='flex flex-col justify-center items-center mb-10'>
+        <img className='max-w-[5rem] aspect-square object-contain' src={noot}/>
+        <h1 className='text-center'>noot & milk's</h1>
+        <div className='bg-pink text-white rounded-lg w-full'>QPI Calculator</div>
+      </div>
+      
+  </>)
+}
+
+function Courses(){
+  return(
+  <>
+  </>
+  )
+}
+
+function YourQPI(){
+  return(
+    <>
+    </>
+  )
+}
+
+function CumulativeQPI(){
+  return(
+  <>
+  </>
   )
 }
 
