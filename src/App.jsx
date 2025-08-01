8abb0c2383040f789d517c48fd158ea8229998e2
neons-amiel <robertamiel.abadilla@gmail.com>
2025-08-01 23:17:06 +0800

    Made the header on the top

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,12 @@ function App() {
 
   return (
     <>
-      <Title />
+      <body className="flex flex-col items-center">
+        <NavBar />
+        <Title />
+        
+      </body>
+      
       
     </>
   )
@@ -21,6 +26,16 @@ function Label({text, bg}){
     <div className={`text-white bg-${bg} rounded-lg px-4 py-2 inline-block`}>
       {text}
     </div>
+  )
+}
+
+function NavBar(){
+  return(
+    <nav className="w-full sticky top-0 z-50 bg-white shadow-sm shadow-fuchsia-300 flex justify-center items-center p-4">
+      <h1 className="text-lg sm:text-xl; md:text-2xl font-bold text-center">
+        NOOT & MILK'S
+      </h1>
+    </nav>
   )
 }
 
