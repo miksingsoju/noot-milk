214769b991c92cbfd768c443062d9b0f4458db9a
neons-amiel <robertamiel.abadilla@gmail.com>
2025-08-02 23:00:13 +0800

    cards are now pretty

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,10 +5,10 @@ function Title({name}){
   return(
   <>  
   <Link to='/'>
-   <div className='flex flex-col justify-center items-center mb-10'>
+   <div className='flex flex-col justify-center items-center mb-10 '>
         <h1 className='text-center pb-3'>noot & milk's</h1>
         <div className='bg-pink text-white rounded-lg w-1/3 p-2 mb-5'>{name}</div>
-        <img className='w-m aspect-square object-contain' src={Noot}/>
+        <img className='w-m aspect-square object-contain hover:scale-105 duration-300' src={Noot}/>
       </div>
 
   </Link>
